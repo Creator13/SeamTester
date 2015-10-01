e63b15b5bea293e100c36fedd0237078f990992c
Casper van Battum <cvanbattum@gmail.com>
2015-10-01 21:48:08 +0200

    Initial commit

---
+++ src/com/cvanbattum/seamtest/SeamTester.java
@@ -0,0 +1,11 @@
+package com.cvanbattum.seamtest;
+
+public class SeamTester {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	
+	
+}
---
+++ src/com/cvanbattum/seamtest/Tester.java
@@ -0,0 +1,24 @@
+package com.cvanbattum.seamtest;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+
+public class Tester {
+	
+	private int step;
+	
+	private final BufferedImage img;
+	
+	public Tester(BufferedImage img) {
+		this.img = img;
+	}
+	
+	public boolean test(int seamOffset) {
+		return false;
+	}
+	
+	private void next() {
+		
+	}
+	
+}
